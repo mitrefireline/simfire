ba0f2e7966cd7eff29b2811701778b9213c6b8ae
Michael G Threet <mthreet@mitre.org>
2021-10-12 13:37:32 +0000

    GPU support now works with rothermel rate of spread computation. The device is auto-selected based on if a CUDA-capable device is detected

--- src/config.py
+++ src/config.py
@@ -1,17 +1,27 @@
 from typing import Tuple
 
+import GPUtil
+import numpy as np
+
 from .world.parameters import FuelArray
 from .world.presets import Chaparral, ShortGrass
 
+# Use GPU if available, else CPU
+try:
+    if len(GPUtil.getAvailable()) > 0:
+        device = 'cuda'
+except:
+    device = 'cpu'
+    
 # Game/Screen parameters
 # Screen size in pixels
-screen_size: int = 225
+screen_size: int = 625
 # Number of terrain tiles in each row/column
-terrain_size: int = 15
+terrain_size: int = 25
 # Fire/flame szie in pixels
 fire_size: int = 2
 # The amount of feet 1 pixel represents (ft)
-pixel_scale = 500
+pixel_scale = 30
 # Copmute the size of each terrain tile in feet
 terrain_scale = terrain_size * pixel_scale
 
@@ -24,7 +34,7 @@ terrain_map: Tuple[Tuple[FuelArray]] = ((chaparral_row, ) * (terrain_size // 2) 
 
 # Fire Manager Parameters
 # (x, y) starting coordinates
-fire_init_pos: Tuple[int, int] = (112, 112)
+fire_init_pos: Tuple[int, int] = (110, 110)
 # Fires burn for 7 frames
 max_fire_duration: int = 10
 
@@ -33,6 +43,6 @@ max_fire_duration: int = 10
 M_f: float = 0.03
 # Wind Speed (ft/min)
 # ft/min = 88*mi/hour
-U: float = 88 * 5
+U: float = 88 * 2
 # Wind Direction (degrees clockwise from north)
 U_dir: float = 45
--- src/world/rothermel.py
+++ src/world/rothermel.py
@@ -1,55 +1,58 @@
 from math import atan2, cos, exp, radians
+from numba import vectorize
 from typing import Tuple
 
-from src.world.parameters import Environment, FuelArray, FuelParticle
+from .parameters import Environment, FuelArray, FuelParticle
+from ..config import device
 
 
-def compute_rate_of_spread(loc: Tuple[float, float, float], new_loc: Tuple[float, float,
-                                                                           float],
-                           fuel_arr: FuelArray, fuel_particle: FuelParticle,
-                           environment: Environment) -> float:
+@vectorize([('float32(float32,float32,float32,float32,float32,float32,'
+            'float32,float32,float32,float32,float32,float32,'
+            'float32,float32,float32,float32,float32)')], target=device)
+def compute_rate_of_spread(loc_x: float, loc_y: float, loc_z: float,
+                           new_loc_x: float, new_loc_y: float, new_loc_z: float,
+                           w_0: float, delta: float, M_x: float, sigma: float, 
+                           h: float, S_T: float, S_e: float, p_p: float,
+                           M_f: float, U: float, U_dir: float) -> float:
     # Mineral Damping Coefficient
-    eta_S = min(0.174 * fuel_particle.S_e**-0.19, 1)
+    eta_S = min(0.174 * S_e**-0.19, 1)
     # Moisture Damping Coefficient
-    r_M = min(environment.M_f / fuel_arr.fuel.M_x, 1)
-    eta_M = 1 - 2.59 * r_M + 5.11 * r_M**2 - 3.52 * r_M**3
+    r_M = min(M_f / M_x, 1)
+    eta_M = 1 - 2.59*r_M + 5.11*r_M**2 - 3.52*r_M**3
     # Net Fuel Load (lb/ft^2)
-    w_n = fuel_arr.fuel.w_0 * (1 - fuel_particle.S_T)
+    w_n = w_0 * (1 - S_T)
     # Oven-dry Bulk Density (lb/ft^3)
-    p_b = fuel_arr.fuel.w_0 / fuel_arr.fuel.delta
+    p_b = w_0 / delta
     # Packing Ratio
-    B = p_b / fuel_particle.p_p
+    B = p_b / p_p
     # Optimum Packing Ratio
-    B_op = 3.348 * fuel_arr.fuel.sigma**-0.8189
+    B_op = 3.348 * sigma**-0.8189
     # Maximum Reaction Velocity (1/min)
-    gamma_prime_max = fuel_arr.fuel.sigma**1.5 / (495 + 0.0594 * fuel_arr.fuel.sigma**1.5)
-    A = 133 * fuel_arr.fuel.sigma**-0.7913
+    gamma_prime_max = sigma**1.5 / (495 + 0.0594*sigma**1.5)
+    A = 133 * sigma**-0.7913
     # Optimum Reaction Velocity (1/min)
     gamma_prime = gamma_prime_max * (B / B_op)**A * exp(A * (1 - B / B_op))
     # Reaction Intensity (BTU/ft^2-min)
-    I_R = gamma_prime * w_n * fuel_particle.h * eta_M * eta_S
+    I_R = gamma_prime * w_n * h * eta_M * eta_S
     # Propagating Flux Ratio
-    xi = exp((0.792 + 0.681 * fuel_arr.fuel.sigma**0.5) *
-             (B + 0.1)) / (192 + 0.25 * fuel_arr.fuel.sigma)
+    xi = exp((0.792 + 0.681*sigma**0.5) * (B + 0.1)) / (192 + 0.25*sigma)
 
     # Wind Factor
-    c = 7.47 * exp(-0.133 * fuel_arr.fuel.sigma**0.55)
-    b = 0.02526 * fuel_arr.fuel.sigma**0.54
-    e = 0.715 * exp(-3.59e-4 * fuel_arr.fuel.sigma)
+    c = 7.47 * exp(-0.133*sigma**0.55)
+    b = 0.02526 * sigma**0.54
+    e = 0.715 * exp(-3.59e-4 * sigma)
     # Need to find wind component in direction of travel
     # Switch order of y-component subtraction since image y coordintates
     # increase from top to bottom
-    angle_of_travel = atan2(loc[1] - new_loc[1], new_loc[0] - loc[0])
+    angle_of_travel = atan2(loc_y-new_loc_y, new_loc_x-loc_x)
     # Subtract 90 degrees because this angle is North-oriented
-    wind_angle_radians = radians(90 - environment.U_dir)
-    wind_along_angle_of_travel = environment.U * \
-                                 cos(wind_angle_radians - angle_of_travel)
+    wind_angle_radians = radians(90-U_dir)
+    wind_along_angle_of_travel = U * \
+                                 cos(wind_angle_radians-angle_of_travel)
     # This is the wind speed in in this direction
     U = wind_along_angle_of_travel
     # Negative wind leads to trouble with calculation and doesn't
     # physically make sense
-    # theta_t_d = math.degrees(angle_of_travel)
-    # theta_w_d = math.degrees(wind_angle_radians)
     U = max(U, 0)
     phi_w = c * U**b * (B / B_op)**-e
 
@@ -58,12 +61,12 @@ def compute_rate_of_spread(loc: Tuple[float, float, float], new_loc: Tuple[float
     # We can approximate this using the z coordinates for each point
     # This equation normally has tan(phi)**2, but we can substitute
     # tan(phi) = (new_loc.z-old_loc.z)
-    phi_s = 5.275 * B**-0.3 * (new_loc[2] - loc[2])**2
+    phi_s = 5.275 * B**-0.3 * (new_loc_z-loc_z)**2
 
     # Effective Heating Number
-    epsilon = exp(-138 / fuel_arr.fuel.sigma)
+    epsilon = exp(-138/sigma)
     # Heat of Preignition (BTU/lb)
-    Q_ig = 250 + 1116 * environment.M_f
+    Q_ig = 250 + 1116*M_f
 
     # Rate of Spread (ft/min)
     R = (I_R * xi * (1 + phi_w + phi_s)) / (p_b * epsilon * Q_ig)

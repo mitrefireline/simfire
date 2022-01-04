12a7d132146d51ac9ece44fcd0da6ce81109b495
mdotter <mdotter@mitre.org>
2022-01-04 06:12:37 +0000

    update script to run simulation and rl harness with new Simulation class and RL Harness class

--- game_rl.py
+++ game_rl.py
@@ -1,33 +1,11 @@
-import pygame
 import random
 from pathlib import Path
 from src.utils.config import Config
-from src.rl_env.fireline_env import FireLineEnv, RLEnv
 from src.rl_env.harness import RLEnvironment
 from src.rl_env.simulation import RothermalSimulation
 
 
 def main():
-    cfg_path = Path('./config.yml')
-    cfg = Config(cfg_path)
-    seed = None
-    if seed is not None:
-        env_config = FireLineEnv(cfg, seed)
-    else:
-        env_config = FireLineEnv(cfg)
-    rl_environment = RLEnv(env_config)
-    state = rl_environment.reset()
-    done = False
-    final_reward = 0
-    while not done:
-        action = some_action_func(state)
-        state, reward, done, _ = rl_environment.step(action)
-        final_reward += reward
-
-    pygame.quit()
-
-
-def main_v2():
     cfg_path = Path('./config.yml')
     cfg = Config(cfg_path)
 
@@ -55,4 +33,4 @@ def some_action_func(state):
 
 
 if __name__ == '__main__':
-    main_v2()
+    main()

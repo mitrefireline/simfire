e6446ae289a4246f202ee2980d2cac5b89b834ab
mdotter <mdotter@mitre.org>
2022-03-18 16:12:26 +0000

    WIP: refactoring tests for DataLayer() and TopographyLayer()

---
+++ src/utils/_tests/test_layers.py
@@ -0,0 +1,149 @@
+import unittest
+from ..layers import TopographyLayer
+
+
+class TestTopographyLayer(unittest.TestCase):
+    def setUp(self) -> None:
+        '''
+        Set up tests for reading in real topography
+        '''
+
+    def test__get_nearest_tile(self) -> None:
+        '''
+        Test that the call to _get_nearest_tile() runs properly.
+        This method will calculate the closest 5 degrees (N, W) that
+            contain the latitude and longitude specified
+        '''
+        resolution = 30
+
+        # 2 Tiles
+        center = (32.4, 115.004)
+        height, width = 1600, 1600
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        test_output_dems = ((32, 116), (32, 114))
+        self.assertEqual(topographyGen.five_deg_north_min, test_output_dems[0][0])
+        self.assertEqual(topographyGen.five_deg_north_max, test_output_dems[1][0])
+        self.assertEqual(topographyGen.five_deg_west_min, test_output_dems[1][1])
+        self.assertEqual(topographyGen.five_deg_west_max, test_output_dems[0][1])
+
+        # 4 Tiles
+        center = (33.001, 115.001)
+        height, width = 1600, 1600
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        test_output_dems = ((32, 116), (32, 115), (33, 115), (34, 116))
+        self.assertEqual(topographyGen.five_deg_north_min, test_output_dems[0][0])
+        self.assertEqual(topographyGen.five_deg_north_max, test_output_dems[2][0])
+        self.assertEqual(topographyGen.five_deg_west_min, test_output_dems[1][1])
+        self.assertEqual(topographyGen.five_deg_west_max, test_output_dems[0][1])
+
+    def test__stack_tiles(self) -> None:
+        '''
+        Test that the call to _stack_tiles() runs properly.
+        This method correctly stitches together tiles on (easternly, southernly, square)
+        '''
+
+        # 2 Tiles
+        resolution = 30
+        center = (32.001, 115.6)
+        height, width = 1600, 1600
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        test_output_dems = {'north': ((31, 116), (32, 116))}
+        self.assertEqual(test_output_dems, topographyGen.tiles)
+
+        # 4 Tiles
+        resolution = 90
+        center = (34.99, 110.001)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        test_output_dems = {'square': ((30, 115), (30, 110), (40, 110), (40, 115))}
+        self.assertEqual(test_output_dems, topographyGen.tiles)
+
+    def test__generate_lat_long(self) -> None:
+        '''
+        Test that the call to _genrate_lat_long() runs properly.
+        This method first creates an array of the latitude and longitude coords of all
+            DEMs contained withing the specified lat/long region when MERITLayer
+            is initialized. It will correctly calculate the corners of the array
+            and fill the array with lat/long pairs using 5degs/6000 pixels
+            or ~3 arc-seconds at every point.
+        It will then find the correct bounds of the specified lat/long region to
+            pull elevation data.
+        '''
+        resolution = 30
+
+        # # 2 Tiles easternly
+        center = (32.4, 115.01)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        self.assertEqual(topographyGen.elev_array.shape, (7224, 3612, 2))
+
+        # 2 Tiles northernly
+        center = (32.001, 115.6)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        self.assertEqual(topographyGen.elev_array.shape, (3612, 7224, 2))
+
+    def test__get_lat_long_bbox(self) -> None:
+        '''
+        Test that the call to _get_lat_long_bbox() runs properly.
+        This method will update the corners of the array of DEM tiles loaded
+
+        '''
+        resolution = 30
+
+        # 2 Tiles northernly
+        center = (32.001, 115.6)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        output = [(31, 116), (31, 115), (33, 115), (33, 116)]
+        self.assertEqual(output, topographyGen.corners)
+
+    def test_save_contour_map(self) -> None:
+        '''
+        Test that the call to _save_contour_map() runs propoerly.
+        '''
+        pass
+
+    def test__make_contour_and_data(self) -> None:
+        '''
+        Test that the call to _generate_contours() runs propoerly.
+        This method returns the data array containing the elevations within the
+            specified bounding box region of the given latitudes and longitudes.
+
+        NOTE: This method should always return a square
+        '''
+        resolution = 30
+        # 2 Tiles (easternly)
+        center = (33.4, 115.04)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        data_layer = topographyGen._make_contour_and_data()
+        self.assertEqual(data_layer.shape[0], data_layer.shape[1])
+
+    def test__get_dems(self) -> None:
+        '''
+        Test that the call to _get_dems() runs properly.
+        This method will generate a list of the DEMs in the fireline /nfs/
+        '''
+
+        resolution = 30
+        # Single Tile
+        center = (32.2, 115.6)
+        height, width = 1600, 1600
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        topographyGen._get_dems()
+        self.assertEqual(1, len(topographyGen.tif_filenames))
+
+        # 2 Tiles
+        center = (32.4, 115.0)
+        height, width = 1600, 1600
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        topographyGen._get_dems()
+        self.assertEqual(2, len(topographyGen.tif_filenames))
+
+        # 4 Tiles
+        center = (34.001, 116.008)
+        height, width = 3200, 3200
+        topographyGen = TopographyLayer(center, height, width, resolution)
+        topographyGen._get_dems()
+        self.assertEqual(4, len(topographyGen.tif_filenames))

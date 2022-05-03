Fix bug with consant value wind. The normalization would lead to a divide by zero for "simple" wind cases

src/game/game.py
@@ -207,16 +207,21 @@ class Game():
         Returns:
             The PyGame Surface for the wind magnitude
         '''
-        wind_mag_surf = pygame.Surface(self.screen.get_size())
         w_max = np.amax(wind_magnitude_map)
         w_min = np.amin(wind_magnitude_map)
         wind_speed_range = (w_max - w_min)
-        for y_idx, y in enumerate(wind_magnitude_map):
-            for x_idx, x in enumerate(y):
-                w_mag = x
-                gradient = (255 - 0)
-                color_mag = int((((w_mag - w_min) * gradient) / wind_speed_range) + 0)
-                wind_mag_surf.set_at((x_idx, y_idx), pygame.Color(0, color_mag, 0))
+        # Constant value wind at all locations. Set everything to middle value
+        if wind_speed_range == 0:
+            color_arr = np.full(self.screen.get_size(), 127, dtype=np.uint8)
+            wind_mag_surf = pygame.surfarray.make_surface(color_arr.swapaxes(0, 1))
+        else:
+            wind_mag_surf = pygame.Surface(self.screen.get_size())
+            for y_idx, y in enumerate(wind_magnitude_map):
+                for x_idx, x in enumerate(y):
+                    w_mag = x
+                    gradient = (255 - 0)
+                    color_mag = int(((w_mag - w_min) * gradient) / (wind_speed_range) + 0)
+                    wind_mag_surf.set_at((x_idx, y_idx), pygame.Color(0, color_mag, 0))
         return wind_mag_surf
 
     def _get_wind_dir_surf(self, wind_direction_map: Sequence[Sequence[float]]) -> \
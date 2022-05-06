bbc46c4ed50abd2e6a31f0bd10290ba01082c48f
Michael Threet <mthreet@mitre.org>
2022-05-06 16:54:51 +0000

    Update code to use new perlin elevation function

--- src/world/elevation_functions.py
+++ src/world/elevation_functions.py
@@ -1,15 +1,37 @@
 from math import exp
-from typing import Callable, Tuple
+from typing import Callable
 
-import numpy as np
-
-from ..enums import ElevationConstants
+import noise
 
 ElevationFn = Callable[[int, int], float]
 
 
+def flat() -> ElevationFn:
+    """
+    Create a callable that returns 0 for all elevations.
+
+    Returns:
+        A callable that computes z values for (x, y) inputs
+    """
+
+    def fn(x: int, y: int) -> float:
+        """
+        Return a constant, flat elevation value at every x and y point
+
+        Arguments:
+            x: The input x location (isn't used).
+            y: The input y location (isn't used).
+
+        Returns:
+            The constant, flat elevation of 0.
+        """
+        return 0
+
+    return fn
+
+
 def gaussian(
-    amplitude: int, mu_x: int, mu_y: int, sigma_x: int, sigma_y: int
+    amplitude: float, mu_x: float, mu_y: float, sigma_x: float, sigma_y: float
 ) -> ElevationFn:
     """
     Create a callable that returns the value of a Gaussian centered at (mu_x, mu_y) with
@@ -31,11 +53,11 @@ def gaussian(
         Return the gaussian function value at the specified point.
 
         Arguments:
-            x: The input x coordinate
-            y: The input y coordinate
+            x: the input x coordinate
+            y: the input y coordinate
 
         Returns:
-            z: The output z coordinate computed by the function
+            The output z coordinate computed by the function
         """
 
         exp_term = ((x - mu_x) ** 2 / (4 * sigma_x**2)) + (
@@ -47,142 +69,54 @@ def gaussian(
     return fn
 
 
-class PerlinNoise2D:
-    def __init__(
-        self,
-        amplitude: float,
-        shape: Tuple[int, int],
-        resolution: Tuple[int, int],
-        seed: int = None,
-    ) -> None:
-        """
-        Create a class to compute perlin noise for given input parameters.
-
-        Arguments:
-            amplitude: The amplitude to scale the noise by.
-            shape: The output shape of the data.
-            resolution: The resolution of the noise.
-            seed: The initialization seed for randomization.
-        """
-        self.amplitude = amplitude
-        self.shape = shape
-        self.resolution = resolution
-        self.seed = seed
-        self.terrain_map = self._precompute()
-
-    def _precompute(self) -> np.ndarray:
-        """
-        Precompute the noise at each (x, y) location for faster use later.
-        """
-
-        def f(t):
-            return 6 * t**5 - 15 * t**4 + 10 * t**3
-
-        delta = (self.resolution[0] / self.shape[0], self.resolution[1] / self.shape[1])
-        d = (self.shape[0] // self.resolution[0], self.shape[1] // self.resolution[1])
-        # Ignore mypy here becuase it thinks the floats in delta are indexing an array,
-        # but this is the intended functionality of np.mgrid()
-        grid = (
-            np.mgrid[
-                0 : self.resolution[0] : delta[0],  # type: ignore
-                0 : self.resolution[1] : delta[1],  # type: ignore
-            ].transpose(  # type: ignore
-                1, 2, 0
-            )
-            % 1
-        )  # type: ignore
-        # Gradients
-        if isinstance(self.seed, int):
-            np.random.seed(self.seed)
-        angles = (
-            2 * np.pi * np.random.rand(self.resolution[0] + 1, self.resolution[1] + 1)
-        )
-        gradients = np.dstack((np.cos(angles), np.sin(angles)))
-        g00 = gradients[0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
-        g10 = gradients[1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
-        g01 = gradients[0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1)
-        g11 = gradients[1:, 1:].repeat(d[0], 0).repeat(d[1], 1)
-        # Ramps
-        n00 = np.sum(grid * g00, 2)
-        n10 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1])) * g10, 2)
-        n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1] - 1)) * g01, 2)
-        n11 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1] - 1)) * g11, 2)
-        # Interpolation
-        t = f(grid)
-        n0 = n00 * (1 - t[:, :, 0]) + t[:, :, 0] * n10
-        n1 = n01 * (1 - t[:, :, 0]) + t[:, :, 0] * n11
-        terrain_map = (
-            self.amplitude * np.sqrt(2) * ((1 - t[:, :, 1]) * n0 + t[:, :, 1] * n1)
-        )
-        terrain_map = terrain_map + np.min(terrain_map)
-        # Add the mean elevation of CA to the map
-        terrain_map = terrain_map + ElevationConstants.MEAN_ELEVATION
-        # Apply bounds to the map to prevent elevation values from being greater or less
-        # than what is possible for the state
-        terrain_map = self._apply_elevation_bounds(terrain_map)
-
-        return terrain_map
-
-    def _apply_elevation_bounds(self, elevation_map: np.ndarray) -> np.ndarray:
-        """
-        Apply elevation bounds to the elevation map.
-
-        Arguments:
-            elevation_map: The elevation map to apply the bounds to
-
-        Returns:
-            The elevation map with bounds applied
-        """
-        min_elevation = ElevationConstants.MIN_ELEVATION
-        max_elevation = ElevationConstants.MAX_ELEVATION
-        elevation_map[elevation_map < min_elevation] = min_elevation
-        elevation_map[elevation_map > max_elevation] = max_elevation
-        return elevation_map
-
-    def get_fn(self) -> ElevationFn:
-        """
-        Get the the elevation function based on the noise.
-        Returns:
-            The function that maps (x, y) points in the terrain to
-            elevations.
-        """
-
-        def fn(x: int, y: int) -> float:
-            """
-            Wrapper function to retrieve the perlin noise values at input (x, y)
-            coordinates.
-
-            Arguments:
-                x: The x coordinate to retrieve
-                y: The y coordinate to retrieve
-
-            Returns:
-                The perlin noise value at the (x, y) coordinates
-            """
-            return self.terrain_map[x, y]
-
-        return fn
-
-
-def flat() -> ElevationFn:
+def perlin(
+    octaves: int,
+    persistence: float,
+    lacunarity: float,
+    seed: int,
+    range_min: float,
+    range_max: float,
+) -> ElevationFn:
     """
-    Create a callable that returns 0 for all elevations.
+    Create a callable that returns the value of a 2D Perlin noise function.
+
+    Arguments:
+        octaves: specifies the number of passes, defaults to 1 (simple noise).
+        persistence: specifies the amplitude of each successive octave relative
+                     to the one below it. Defaults to 0.5 (each higher octave's amplitude
+                     is halved). Note the amplitude of the first pass is always 1.0.
+        lacunarity: specifies the frequency of each successive octave relative
+                    to the one below it, similar to persistence. Defaults to 2.0.
+        seed: The seed to used to generate random terrain. `seed` takes the place of the
+              `base` argument in the `snoise2()` function, which adds offsets to the
+              input (x, y) coordinates to get new terrain
+        range_min: The minimum amplitude to scale to
+        range_max: The maximum amplitude to scale to
 
     Returns:
-        A callable that computes z values for (x, y) inputs
+        A callable that computes Perlin Noise z-values for (x, y) inputs
     """
+    if range_min >= range_max:
+        raise ValueError(f"range_min={range_min} must be less than range_max={range_max}")
 
     def fn(x: int, y: int) -> float:
         """
-        Return a constant, flat elevation value at every x and y point
+        Return the generated Perlin Noise function at the specified value.
 
         Arguments:
-            x: The input x location (isn't used).
-            y: The input y location (isn't used).
+            x: the input x coordinate
+            y: the input y coordinate
 
         Returns:
-            The constant, flat elevation.
+            The output z coordinate computed by the function
         """
-        return 0
+        z = noise.snoise2(x, y, octaves, persistence, lacunarity, base=seed)
+        # Normalize to [0, 1]
+        z = (z + 1) / 2
+        # Scale to [0, range_max-range_min]
+        z = z * (range_max - range_min)
+        # Add to normalize to [range_min, range_max]
+        z = z + range_min
+        return z
 
     return fn

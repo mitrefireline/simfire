bc523238b3a9c335c696d697ce93e9d2061480fc
Michael Doyle <mdoyle@mitre.org>
2022-06-15 23:39:27 +0000

    Add furo theme to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ exclude_patterns = ["**/_tests"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_logo = "../../assets/icons/rl_logo.png"
 html_theme_options = {
     "logo_only": False,

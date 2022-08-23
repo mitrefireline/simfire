35e05d8276b415c8917e96dc64aa02bed8e14416
Michael Threet <mthreet@mitre.org>
2022-08-23 16:27:41 +0000

    Importing simfire now imports all submodules
    There was also some aliasing done for redundancies
    
    Example: simfire.game.game.Game -> simfire.game.Game

---
+++ simfire/game/managers/__init__.py
@@ -0,0 +1,7 @@
+from .fire import ConstantSpreadFireManager  # noqa: F401
+from .fire import RoSAttenuation  # noqa: F401
+from .fire import RothermelFireManager  # noqa: F401
+from .mitigation import ControlLineManager  # noqa: F401
+from .mitigation import FireLineManager  # noqa: F401
+from .mitigation import ScratchLineManager  # noqa: F401
+from .mitigation import WetLineManager  # noqa: F401
